1f30f0888eaf056daa8eda0855c3c5b93d1b192b
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:16:51 +0300

    Create t_User.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Domain/Entities/t_User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskManagementProject.Domain.Entities.Base;
+
+namespace TaskManagementProject.Domain.Entities
+{
+    public class t_User : EntityBase
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
