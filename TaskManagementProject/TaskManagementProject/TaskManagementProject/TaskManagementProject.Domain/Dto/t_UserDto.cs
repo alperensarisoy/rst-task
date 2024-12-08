fe15f24e810db6031ad69472d6186492d022b4fd
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:16:25 +0300

    Create t_UserDto.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Domain/Dto/t_UserDto.cs
@@ -0,0 +1,13 @@
+﻿using TaskManagementProject.Domain.Dto.Base;
+
+namespace TaskManagementProject.Domain.Dto
+{
+    public class t_UserDto : DtoBase
+    {
+
+        public string Name { get; set; }    
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
