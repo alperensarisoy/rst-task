72cbd249f72e563fa449b8c84b08a0ee918f3a9f
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:17:01 +0300

    Create PasswordHasher.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Infrastructure/Concrete/PasswordHasher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementProject.Infrastructure.Interfaces;
+
+namespace TaskManagementProject.Infrastructure.Concrete
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower(); // Hashlenmiş şifreyi döndürür
+            }
+        }
+
+        public bool VerifyPassword(string password, string hashedPassword)
+        {
+            var hashOfInput = HashPassword(password);
+            if(hashOfInput.Trim() == hashedPassword.Trim())
+            {
+                return true;
+            }
+            return false;
+            // Şifreyi doğrulamak için hash karşılaştırması
+        }
+    }
+}
