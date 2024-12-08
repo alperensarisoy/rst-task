f43b244c0a2f384b63bc4f8a98c8863bbe8ddbb0
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:17:08 +0300

    Create ITaskManagementProjectMapper.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Infrastructure/AutoMapper/ITaskManagementProjectMapper.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagementProject.Infrastructure.AutoMapper
+{
+    public interface ITaskManagementProjectMapper
+    {
+        TDestination Map<TDestination>(object source);
+        object Map(object source, Type sourceType, Type destinationType);
+
+        TEntity Map<TSource, TEntity>(TSource source, TEntity destination);
+    }
+}
