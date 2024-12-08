c0dbb59b00b21c8a135987f825e405c644af89f0
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:16:49 +0300

    Create t_Task.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Domain/Entities/t_Task.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskManagementProject.Domain.Entities.Base;
+
+namespace TaskManagementProject.Domain.Entities
+{
+    public class t_Task : EntityBase
+    {
+        [Required]
+        public string Title { get; set; }   
+
+        public string? Description { get; set; } 
+
+    }
+}
