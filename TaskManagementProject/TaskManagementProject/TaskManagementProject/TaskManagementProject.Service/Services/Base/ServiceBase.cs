a460910626d31e1c5f22314831f968e0cbfb259e
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:17:12 +0300

    Create ServiceBase.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Service/Services/Base/ServiceBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using TaskManagementProject.Data.Interfaces;
+using TaskManagementProject.Infrastructure.AutoMapper;
+
+namespace TaskManagementProject.Service.Services.Base
+{
+    public class ServiceBase
+    {
+
+        public readonly IUnitOfWork _unitOfWork;
+        public readonly ITaskManagementProjectMapper _mapper;
+        public readonly IHttpContextAccessor _httpContext;
+
+        public ServiceBase(IUnitOfWork unitOfWork , ITaskManagementProjectMapper mapper, IHttpContextAccessor httpContext)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _httpContext = httpContext; 
+            
+        }
+    }
+}
