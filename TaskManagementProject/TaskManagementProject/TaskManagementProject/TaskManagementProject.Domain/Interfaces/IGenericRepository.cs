c245b5e47e17f4a3293220192fc001c67a97e166
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:16:43 +0300

    Create IGenericRepository.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace TaskManagementProject.Domain.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class, new()
+    {
+        Task<IQueryable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> filter = null);
+
+        Task<TEntity> GetByIdAsync(long id);
+
+        Task<bool> DeleteByIdAsync(long id);
+        
+        Task<TEntity> AddAsync(TEntity entity);
+
+        Task<TEntity> UpdateAsync(TEntity entity);
+    }
+}
