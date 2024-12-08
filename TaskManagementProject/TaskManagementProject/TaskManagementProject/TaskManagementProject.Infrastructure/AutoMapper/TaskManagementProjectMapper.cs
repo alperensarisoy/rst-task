f1a9378a36df2e263a7704c96524ee8de7654ab5
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:17:03 +0300

    Create TaskManagementProjectMapper.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Infrastructure/AutoMapper/TaskManagementProjectMapper.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+
+namespace TaskManagementProject.Infrastructure.AutoMapper
+{
+    public class TaskManagementProjectMapper : ITaskManagementProjectMapper
+    {
+        private readonly IMapper _mapper;
+
+        public TaskManagementProjectMapper()
+        {
+            _mapper = MappingProfile.InitializeAutoMapper().CreateMapper();
+        }
+
+        // Tek parametreli Map metodu (zaten vardı)
+        public TDestination Map<TDestination>(object source)
+        {
+            return _mapper.Map<TDestination>(source);
+        }
+
+        // Üç parametreli Map metodu (zaten vardı)
+        public object Map(object source, Type sourceType, Type destinationType)
+        {
+            return _mapper.Map(source, sourceType, destinationType);
+        }
+
+        // Yeni eklenen iki parametreli Map metodu
+        public TEntity Map<TSource, TEntity>(TSource source, TEntity destination)
+        {
+            return _mapper.Map(source, destination);
+        }
+    }
+}
