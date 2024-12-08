3777b80506377919ffae2a00739c691a53f40377
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:16:19 +0300

    Create IUserRepository.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Data/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+using TaskManagementProject.Domain.Entities;
+using TaskManagementProject.Domain.Interfaces;
+
+namespace TaskManagementProject.Data.Interfaces
+{
+    public interface IUserRepository : IGenericRepository<t_User>
+    {
+        public  Task<t_User> GetByAsync(Expression<Func<t_User, bool>> filter);
+    }
+}
