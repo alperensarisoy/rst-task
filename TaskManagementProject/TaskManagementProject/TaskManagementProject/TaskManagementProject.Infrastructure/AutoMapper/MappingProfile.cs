6b756c868355c991ef02e02378e22cb5a2549bc7
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:17:04 +0300

    Create MappingProfile.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Infrastructure/AutoMapper/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using TaskManagementProject.Domain.AutoMapper;
+
+namespace TaskManagementProject.Infrastructure.AutoMapper
+{
+    public class MappingProfile
+    {
+        public static MapperConfiguration InitializeAutoMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfile());
+
+            });
+
+            return config;
+        }
+    }
+}
