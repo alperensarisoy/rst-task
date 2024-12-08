944530f3129ec30730250fd83d94f82997eca9bd
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:16:39 +0300

    Create TaskRepository.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Data/Repositories/Domain/TaskRepository.cs
@@ -0,0 +1,17 @@
+﻿using TaskManagementProject.Data.Context;
+using TaskManagementProject.Data.Generics;
+using TaskManagementProject.Data.Interfaces;
+using TaskManagementProject.Domain.Entities;
+
+namespace TaskManagementProject.Data.Repositories.Domain
+{
+    public class TaskRepository : GenericRepository<t_Task>, ITaskRepository
+    {
+
+        private readonly TaskManagementProjectContext _context;
+        public TaskRepository(TaskManagementProjectContext context) : base(context)
+        {
+            _context = context; 
+        }
+    }
+}
