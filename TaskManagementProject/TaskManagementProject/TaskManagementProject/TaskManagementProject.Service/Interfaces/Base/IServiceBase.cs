08f76b1ff41abc0368ad0391f26579e507852ec7
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:16:53 +0300

    Create IServiceBase.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Service/Interfaces/Base/IServiceBase.cs
@@ -0,0 +1,18 @@
+﻿using TaskManagementProject.Domain.Interfaces;
+
+namespace TaskManagementProject.Service.Interfaces.Base
+{
+    public interface IServiceBase<TDto, TEntity>
+    {
+        Task<ICollection<TDto>> GetAllAsync(Func<IEntity, bool> filter = null);
+
+        Task<TDto> GetByIdAsync(int id);
+
+        Task<TDto> AddAsync(TDto dto);
+
+        Task<TDto> UpdateAsync(TDto dto);
+
+        Task<bool> DeleteByAsync(TDto dto);
+
+    }
+}
