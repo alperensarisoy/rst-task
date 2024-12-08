ef161d4240ee8c007e6da1fe1210e3faea3ee008
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:16:21 +0300

    Create TaskProjectContext.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Data/Models/TaskProjectContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskManagementProject.Data.Models;
+
+public partial class TaskProjectContext : DbContext
+{
+    public TaskProjectContext()
+    {
+    }
+
+    public TaskProjectContext(DbContextOptions<TaskProjectContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TTask> TTasks { get; set; }
+
+    public virtual DbSet<TUser> TUsers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-V90BU82;Database=TaskProject;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TTask>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_Task");
+
+            entity.ToTable("t_Tasks");
+
+            entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Description).HasMaxLength(250);
+            entity.Property(e => e.Title).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<TUser>(entity =>
+        {
+            entity.ToTable("t_User");
+
+            entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsFixedLength();
+            entity.Property(e => e.Password)
+                .HasMaxLength(50)
+                .IsFixedLength();
+            entity.Property(e => e.Username)
+                .HasMaxLength(50)
+                .IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
