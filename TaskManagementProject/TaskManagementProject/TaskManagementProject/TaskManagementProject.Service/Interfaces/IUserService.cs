d2662d9bb8bfa064349a5ad867b9e6e0885aacd3
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:17:06 +0300

    Create IUserService.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Service/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using TaskManagementProject.Domain.Entities;
+using TaskManagementProject.Service.Interfaces.Base;
+
+namespace TaskManagementProject.Service.Interfaces
+{
+    public interface IUserService<TDto, TEntity>: IServiceBase<TDto,TEntity>   
+    {
+        Task<t_User> ValidateUserAsync(string username, string password);
+        string GenerateJwtToken(t_User user);
+    }
+}
