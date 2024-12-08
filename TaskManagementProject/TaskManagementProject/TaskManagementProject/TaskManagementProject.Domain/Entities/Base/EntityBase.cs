1f862a255a803594bee58a367ac71743d22d140f
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:16:33 +0300

    Create EntityBase.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Domain/Entities/Base/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using TaskManagementProject.Domain.Interfaces;
+
+namespace TaskManagementProject.Domain.Entities.Base
+{
+    public class EntityBase : IEntity
+    {
+        public long Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
