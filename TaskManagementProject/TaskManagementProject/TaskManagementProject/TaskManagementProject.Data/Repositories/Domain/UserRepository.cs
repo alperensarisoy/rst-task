d0cb1e81963b3311daa0158b37325aa384f61339
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:16:38 +0300

    Create UserRepository.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Data/Repositories/Domain/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using TaskManagementProject.Data.Context;
+using TaskManagementProject.Data.Generics;
+using TaskManagementProject.Data.Interfaces;
+using TaskManagementProject.Domain.Entities;
+
+namespace TaskManagementProject.Data.Repositories.Domain
+{
+    public class UserRepository : GenericRepository<t_User>, IUserRepository
+    {
+
+        private readonly TaskManagementProjectContext
+            _context;
+        public UserRepository(TaskManagementProjectContext context) : base(context)
+        {
+            _context = context; 
+        }
+
+
+        public async Task<t_User> GetByAsync(Expression<Func<t_User, bool>> filter)
+        {
+            var entity = await _dbSet.FirstOrDefaultAsync(filter);
+
+          
+            return entity;
+        }
+    }
+}
