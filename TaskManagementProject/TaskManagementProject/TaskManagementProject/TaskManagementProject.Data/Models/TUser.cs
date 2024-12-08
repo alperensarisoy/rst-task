c88743c1f1ac6604b5c0dbf185dc7b2240b2bad0
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:16:41 +0300

    Create TUser.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Data/Models/TUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskManagementProject.Data.Models;
+
+public partial class TUser
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+}
