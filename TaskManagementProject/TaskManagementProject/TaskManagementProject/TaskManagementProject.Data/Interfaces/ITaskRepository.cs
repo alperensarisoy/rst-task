8fbdb7eca9da736983843c05e7b6d1a25116c46a
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:16:16 +0300

    Create ITaskRepository.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Data/Interfaces/ITaskRepository.cs
@@ -0,0 +1,9 @@
+﻿using TaskManagementProject.Domain.Entities;
+using TaskManagementProject.Domain.Interfaces;
+
+namespace TaskManagementProject.Data.Interfaces
+{
+    public  interface ITaskRepository : IGenericRepository<t_Task>
+    {
+    }
+}
