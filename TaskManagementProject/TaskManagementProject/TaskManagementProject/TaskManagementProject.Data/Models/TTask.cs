d092d89d90252123b1355763ec3830e8113ddbe8
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:16:23 +0300

    Create TTask.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Data/Models/TTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskManagementProject.Data.Models;
+
+public partial class TTask
+{
+    public long Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+}
