6f3636eb89dfbfda288659b71f9f9069718b668b
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:16:31 +0300

    Create t_TaskDto.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Domain/Dto/t_TaskDto.cs
@@ -0,0 +1,11 @@
+﻿using TaskManagementProject.Domain.Dto.Base;
+
+namespace TaskManagementProject.Domain.Dto
+{
+    public class t_TaskDto : DtoBase
+    {
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
