9a5039be142869bd5836c6ae949ccf80b6e9ed50
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:16:30 +0300

    Create AutoMapperProfile.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Domain/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using TaskManagementProject.Domain.Dto;
+using TaskManagementProject.Domain.Entities;
+
+namespace TaskManagementProject.Domain.AutoMapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+         
+            CreateMap<t_User, t_UserDto>().ReverseMap();
+            CreateMap<t_Task, t_TaskDto>().ReverseMap();
+           
+
+        }
+    }
+}
