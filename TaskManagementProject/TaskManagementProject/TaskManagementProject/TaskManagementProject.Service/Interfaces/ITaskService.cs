7aa229a87b5ddf75d4a4c672b54f34a183e1e9d6
Alperen SARISOY <77502105+alperensarisoy@users.noreply.github.com>
2024-12-08 21:17:00 +0300

    Create ITaskService.cs

---
+++ TaskManagementProject/TaskManagementProject/TaskManagementProject/TaskManagementProject.Service/Interfaces/ITaskService.cs
@@ -0,0 +1,8 @@
+﻿using TaskManagementProject.Service.Interfaces.Base;
+
+namespace TaskManagementProject.Service.Interfaces
+{
+    public interface ITaskService<TDto, TEntity> : IServiceBase<TDto, TEntity>
+    {
+    }
+}
